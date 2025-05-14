cc2476aca78d7064be0bc5046f6dce291b95621b
sdefrancesco <skaterbastian@aol.com>
2025-05-14 11:41:00 -0400

    connect to mongodb, add .env with mongoURI

--- index.js
+++ index.js
@@ -3,17 +3,28 @@ import mongoose from 'mongoose'
 import linkRoutes from './routes/links.js';
 import userRoutes from './routes/users.js';
 import cors from 'cors'
+import dotenv from 'dotenv'
+import bodyParser from 'body-parser';
+
+dotenv.config()
 
 const app = express()
+
 
 
 app.use(express.json())
 app.use(cors())
 
+
 app.use('/api/links', linkRoutes)
 app.use('/api/users', userRoutes)
 
+mongoose.connect(process.env.MONGO_URI, {
 
+})
+.then(() => {
+    console.log('MongoDB connected');
+})
 
 // Server
 const PORT = 3000
--- routes/links.js
+++ routes/links.js
@@ -5,7 +5,19 @@ const router = express.Router();
 
 router.get('/', async (req, res) => {
     try {
-        res.status(200).json({ message: 'All links' });
+        let links = await Link.find()
+        res.status(200).json(links);
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to fetch links' });
+    }
+});
+
+router.post('/new', async (req, res) => {
+    try {
+        let newLink = new Link()
+        newLink.link = req.body.link
+        newLink.save()
+        res.status(200).json(req.body);
     } catch (error) {
         res.status(500).json({ error: 'Failed to fetch links' });
     }
